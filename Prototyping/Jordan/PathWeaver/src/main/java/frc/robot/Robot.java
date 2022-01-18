671a90e9e35aa0b91aa811ef3db81cca7f2b4956
LudingtonRobotics <jlake624@gmail.com>
2022-01-18 18:06:12 -0500

    Adding my project

---
+++ Prototyping/Jordan/PathWeaver/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,56 @@
+package frc.robot;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.TimedRobot;
+
+TalonFX new = TalonFX();
+
+
+public class Robot extends TimedRobot {
+
+  String trajectoryJSON = "paths/circle.wpilib.json";
+  Trajectory trajectory = new Trajectory();
+
+  @Override
+  public void robotInit() {
+    try {
+      Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+      trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+   } catch (IOException ex) {
+      DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
+   }
+  }
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+}
