1f3c9b5fda41eff2d6ee1e51940e2cd91889be3b
LudingtonRobotics <jlake624@gmail.com>
2022-02-12 10:33:01 -0500

    Quinn's Changes

---
+++ Prototyping/sam/LEDs/LEDs/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,20 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Joystick;
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+ 
+    
+}
+
+
+
+
---
+++ Prototyping/sam/LEDs/LEDs/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,30 @@
+package frc.robot;
+
+class RobotMap{
+
+    public static final int _rghtMain       =   10;     //Talon FX
+    public static final int _rghtFol1       =   11;     //Talon FX
+    public static final int _leftMain       =   20;     //Talon FX
+    public static final int _leftFol1       =   21;     //Talon FX
+    public static final int shotMotor       =   30;     //Talon FX
+    public static final int _turret         =   31;     //Victor SPX
+    public static final int _endLift        =   35;     //Talon FX
+    public static final int _endLiftServo   =    1;     //Servo
+    public static final int _leveler        =   36;     //Victor SPX
+    public static final int _colrWhel       =   40;     //Victor SPX
+    public static final int LEDs            =    0;      //PWM Spark RoboRio
+    public static final int _intake         =   50;     //Victor SPX
+    public static final int _lowFeed        =   51;     //Spark Max
+    public static final int _upFeed         =   52;     //Spark Max
+    public static final int _ballCollected  =  101;     //Lower Distance Sensor
+    public static final int _intakeFull     =  102;     //Upper Distance Sensor
+
+    
+
+    
+ 
+
+
+
+
+}
